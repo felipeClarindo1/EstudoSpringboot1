32428bbaab4ed4678bba61dc1dce1afe96fa6af5
felipeClarindo1 <144058028+felipeClarindo1@users.noreply.github.com>
2023-11-14 17:44:00 -0300

    Add files via upload

---
+++ desisto/src/test/java/com/api/desisto/DesistoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.desisto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesistoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
